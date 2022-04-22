e78b823f435159f23232618eadb53239e6408ddd
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-04-22 17:01:56 -0300

    Advice Native

--- pages/advice/Advice.jsx
+++ pages/advice/Advice.jsx
@@ -1,37 +1,60 @@
 import { useState } from 'react'
-import { Listbox } from '@headlessui/react'
 import Navbar from "../Navbar"
 import AdviceBody from "./AdviceBody"
 
 const option = [
-  { id: 1, name: 'Investing', unavailable: false },
-  { id: 2, name: 'Buy BTC', unavailable: false },
-  { id: 3, name: 'Sell BTC', unavailable: false },
-  { id: 4, name: 'Can I trust in crypto?', unavailable: false },
-  { id: 5, name: 'Can I use my crypto?', unavailable: false },
+  { id: 0, name: 'Investing', unavailable: false },
+  { id: 1, name: 'Buy BTC', unavailable: false },
+  { id: 2, name: 'Sell BTC', unavailable: false },
+  { id: 3, name: 'Can I trust in crypto?', unavailable: false },
+  { id: 4, name: 'Can I use my crypto?', unavailable: false },
 ]
 
 function Advice() {
-  const [selectedOption, setSelectedOption] = useState(option[0])
+  const [selectedOption, setSelectedOption] = useState(option[2])
+  const handleChange=(e)=>{
+        let i = e.target.value
+        setSelectedOption(option[i])
+    }
+
+  const [open, setOpen] = useState(false)
+  const changeOpen = () =>{
+    const divList = document.getElementById('divList')
+    if(open){
+
+      divList.classList.remove("h-full")
+      divList.classList.add("h-0")
+      setOpen(false)
+    }else{
+
+      divList.classList.remove("h-0")
+      divList.classList.add("h-full")
+      setOpen(true)
+    }
+  }
 
   return (
     <div className="bg-principalColor min-h-screen p-10">
       <Navbar/>
-      <div className="flex flex-col items-center justify-items-start mt-10 h-32 z-10">
-        <Listbox value={selectedOption} onChange={setSelectedOption} >
-          <Listbox.Button className="border-2 rounded-2xl border-secondColor p-2 bg-extraColor w-full sm:w-1/3 flex flex-row justify-between items-center">{selectedOption.name}{/*<SelectorIcon className="w-8"/>*/}</Listbox.Button>
-          <Listbox.Options className="border-2 rounded-2xl border-extraColor p-2 bg-secondColor w-1/3 z-10" >
+      <div className="flex flex-col items-center justify-items-start mt-10 h-32 z-10 w-full">
+        <div  className="flex flex-col w-full z-10 bg-extraColor p-2 rounded-3xl hover:bg-principalColor">
+          <button className="relative border-textColor bg-principalColor text-3xl h-10 overflow-hidden group m-2 hover:border-extraColor border-2 rounded-2xl" onClick={changeOpen}>{selectedOption.name}{/*<SelectorIcon className="w-8"/>*/}</button>
+          <div className="rounded-2xl w-full  flex flex-row flex-wrap justify-between items-center h-0 overflow-hidden duration-300" id="divList">
             {option.map((person) => (
-              <Listbox.Option className="group w-full hover:bg-atentionColor rounded-3xl p-3 flex flex-row justify-between items-center cursor-pointer"
-                key={person.id}
-                value={person}
-                disabled={person.unavailable}
-              >
-                <p className="w-full text-center group-hover:animate-bounce items-center">{person.name}</p> {/*<ArrowCircleLeftIcon className="w-8 opacity-0 group-hover:opacity-100 text-secondColor hover:animate-pulse"/>*/}
-              </Listbox.Option>
+              <div className="relative w-full border-textColor h-10 overflow-hidden group m-2 hover:border-extraColor border-2 rounded-2xl"
+                onClick={changeOpen}>
+                <input className="absolute top-0 left-0 opacity-0 h-screen w-screen z-50 cursor-pointer"
+                  key={person.id}
+                  value={person.id}
+                  onClick={handleChange}
+                >
+                </input>
+                <label className="w-screen bg-secondColor group-hover:bg-atentionColor h-screen text-center absolute top-0 left-0">{person.name}</label>
+                <div className="h-full w-full rounded-full bg-blue-300 group-hover:animate-ping top-0 left-1/6 absolute opacity-0 group-hover:opacity-100"></div>
+              </div>
             ))}
-          </Listbox.Options>
-        </Listbox>
+          </div>
+        </div>
       </div>
       <AdviceBody id={selectedOption}/>
     </div>
--- pages/advice/AdviceBody.jsx
+++ pages/advice/AdviceBody.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 function AdviceBody({id}) {
 
   return (
-    <div>
-      <h2 className="text-center rounded-b-full text-3xl bg-extraColor text-textColor mb-1">{id.name}</h2>
-      <div className="bg-principalColor sticky top-[15%] h-2 m-10 rounded-full"></div>
+    <div className="bg-extraColor p-10 rounded-2xl">
+      <h2 className="text-center rounded-full text-3xl bg-principalColor text-textColor mb-1">{id.name}</h2>
       {
-        (id.id === 1) ?
+        (id.id === 0) ?
         <div className="p-10 m-10 rounded-2xl bg-secondColor min-h-screen flex flex-col justify-around border-2 border-extraColor">
           <p className="hover:bg-atentionColor p-2 rounded">For any investiment you must to know that it have risk.</p>
           <p className="hover:bg-atentionColor p-2 rounded">Anything that you do have a risk. May the company that you bought a stock break. May the country that you invest (tesoury or anything) print a lot of money and your investiment will lost the value.</p>
@@ -17,10 +16,10 @@ function AdviceBody({id}) {
           <p className="hover:bg-atentionColor p-2 rounded">Just do a quicly investiment when you will put a  little amount.</p>
         </div>
         :
-        (id.id === 2) ?
-        <p></p>
+        (id.id === 1) ?
+        <div></div>
         :
-        <p></p>
+        <div></div>
       }
     </div>
   )
